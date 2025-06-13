94e59ae6fc75d67068d04435bc9becee86b28b1a
Srezequielr <julianrierawrk@gmail.com>
2025-06-13 16:02:31 -0300

    clase registro arreglada, se agrego la sintaxis correspondiente de sqlalchemy

--- models/clase_registro.py
+++ models/clase_registro.py
@@ -1,24 +1,30 @@
-import time
-import datetime
+from datetime import datetime, time
+from sqlalchemy import DateTime, Time, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
 
-class registro:
-    __fecha: datetime
-    __hora_entrada: time
-    __hora_salida: time
-    __dependencia: str
-    __id_trabajador: str
-    def __init__(self, fecha, hora_entrada, hora_salida, dependencia):
-        self.__fecha = fecha
-        self.__hora_entrada = hora_salida
-        self.__hora_salida = hora_entrada
-        self.__dependencia = dependencia
+db = SQLAlchemy()
+
+class registro(db.Model):
+    __tablename__= 'registro'
+    __id: Mapped[int] = mapped_column(primary_key=True)
+    __fecha: Mapped[datetime] = mapped_column(DateTime)
+    __hora_entrada: Mapped[time] = mapped_column(Time)
+    __hora_salida: Mapped[time] = mapped_column(Time)
+    __dependencia: Mapped[str] = mapped_column()
+    __id_trabajador: Mapped[int] = mapped_column(ForeignKey("trabajador.id"))
+    
     def get_fecha(self):
         return self.__fecha
+    
     def get_hora_entrada(self):
         return self.__hora_entrada
+    
     def get_hora_salida(self):
         return self.__hora_salida
+    
     def get_dependencia(self):
         return self.__dependencia
+    
     def get_id_trabajador(self):
         return self.__id_trabajador
